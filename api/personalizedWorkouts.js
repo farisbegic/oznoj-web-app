f8d3e261a4e0934a012ef4fb4ab7a9836559c9cf
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-27 12:59:36 +0100

    Sastavljanje prvog plana
    Sastavljanje prvog plana

--- api/personalizedWorkouts.js
+++ api/personalizedWorkouts.js
@@ -1,3 +1,29 @@
 export const pushPullLegs = [{}, {}, {}];
 export const PHUL = [{}, {}, {}];
 export const PHAT = [{}, {}, {}];
+export const PHULWorkout = [
+	{
+		aboutTheProgram: {
+			goal: 'Build Muscle & Strength',
+			workoutType: 'Split',
+			trainingLevel: 'Intermediate',
+			programDuration: '12 weeks',
+			daysperWeek: '4',
+			timePerWorkout: '45-60 minutes',
+			equipment: 'Barbell, Dumbbells, Machines',
+			targetGender: 'Male & Female',
+			recommendedSupps: [
+				'Whey Protein Powder',
+				'Creatine',
+				'Beta-Alanine',
+				'Citrulline Malate',
+				'Pre-workout',
+			],
+		},
+		programData: {
+			day1: {
+				targetMuscleGroup: 'Upper Body',
+			},
+		},
+	},
+];
