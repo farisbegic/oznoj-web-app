7818939b09dc4272066e4e17d655d6126fb7a789
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-27 13:12:52 +0100

    Update personalizedWorkout.js
    Update personalizedWorkout.js

--- api/personalizedWorkouts.js
+++ api/personalizedWorkouts.js
@@ -1,7 +1,4 @@
-export const pushPullLegs = [{}, {}, {}];
-export const PHUL = [{}, {}, {}];
-export const PHAT = [{}, {}, {}];
-export const PHULWorkout = [
+export const PHUL = [
 	{
 		aboutTheProgram: {
 			goal: 'Build Muscle & Strength',
@@ -23,6 +20,93 @@ export const PHULWorkout = [
 		programData: {
 			day1: {
 				targetMuscleGroup: 'Upper Body',
+				trainingType: 'Power',
+				excersises: [
+					{ excersiseName: 'Barbell Bench Press', sets: '3-4', reps: '3-5' },
+					{
+						excersiseName: 'Incline Dumbell Bench Press',
+						sets: '3-4',
+						reps: '6-10',
+					},
+					{ excersiseName: 'Bent Over Row', sets: '3-4', reps: '3-5' },
+					{ excersiseName: 'Lat Pull Down', sets: '3-4', reps: '6-10' },
+					{ excersiseName: 'Overhead Press', sets: '2-3', reps: '5-8' },
+					{ excersiseName: 'Barbell Curl', sets: '2-3', reps: '6-10' },
+					{ excersiseName: 'Skullcrusher', sets: '2-3', reps: '6-10' },
+				],
+			},
+			day2: {
+				targetMuscleGroup: 'Lower Body',
+				trainingType: 'Power',
+				excersises: [
+					{ excersiseName: 'Squat', sets: '3-4', reps: '3-5' },
+					{
+						excersiseName: 'Deadlift',
+						sets: '3-4',
+						reps: '6-10',
+					},
+					{ excersiseName: 'Leg Press', sets: '3-5', reps: '10-15' },
+					{ excersiseName: 'Leg Curl', sets: '3-4', reps: '6-10' },
+					{ excersiseName: 'Calf Excersise', sets: '4', reps: '6-10' },
+				],
+			},
+			day4: {
+				targetMuscleGroup: 'Upper Body',
+				trainingType: 'Hypertrophy',
+				excersises: [
+					{
+						excersiseName: 'Incline Barbell Bench Press',
+						sets: '3-4',
+						reps: '8-12',
+					},
+					{
+						excersiseName: 'Flat Bench Dumbell Fly',
+						sets: '3-4',
+						reps: '8-12',
+					},
+					{ excersiseName: 'Seated Cable Row', sets: '3-4', reps: '8-12' },
+					{ excersiseName: 'One Arm Dumbell Row', sets: '3-4', reps: '8-12' },
+					{ excersiseName: 'Dumbell Lateral Raise', sets: '3-4', reps: '8-12' },
+					{
+						excersiseName: 'Seated Incline Dumbell Curl',
+						sets: '3-4',
+						reps: '8-12',
+					},
+					{
+						excersiseName: 'Cable Tricep Extension',
+						sets: '3-4',
+						reps: '8-12',
+					},
+				],
+			},
+			day5: {
+				targetMuscleGroup: 'Upper Body',
+				trainingType: 'Hypertrophy',
+				excersises: [
+					{
+						excersiseName: 'Incline Barbell Bench Press',
+						sets: '3-4',
+						reps: '8-12',
+					},
+					{
+						excersiseName: 'Flat Bench Dumbell Fly',
+						sets: '3-4',
+						reps: '8-12',
+					},
+					{ excersiseName: 'Seated Cable Row', sets: '3-4', reps: '8-12' },
+					{ excersiseName: 'One Arm Dumbell Row', sets: '3-4', reps: '8-12' },
+					{ excersiseName: 'Dumbell Lateral Raise', sets: '3-4', reps: '8-12' },
+					{
+						excersiseName: 'Seated Incline Dumbell Curl',
+						sets: '3-4',
+						reps: '8-12',
+					},
+					{
+						excersiseName: 'Cable Tricep Extension',
+						sets: '3-4',
+						reps: '8-12',
+					},
+				],
 			},
 		},
 	},
