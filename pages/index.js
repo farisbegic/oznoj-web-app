15a287421cbe99a8c77c42c3ecd350755badd33b
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-11-27 02:36:40 +0100

    Implemented join section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Header from "../components/partials/Header";
 import Hero from "../components/Home/Hero";
 import Services from "../components/Home/Services";
+import Join from "../components/Home/Join";
 export default function Home() {
   return (
       <div>
         <Header />
           <Hero />
           <Services />
+          <Join />
       </div>
   )
 }
