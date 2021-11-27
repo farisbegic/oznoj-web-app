0b5d5f4579dfe2a288f148da6c71237f87f578ad
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-27 14:33:04 +0100

    changes
    changes

--- api/getRecepies.js
+++ api/getRecepies.js
@@ -1,20 +1,15 @@
 var axios = require('axios').default;
 
-var options = {
-	method: 'GET',
-	url: 'https://recipesapi2.p.rapidapi.com/recipes/tomatosoup',
-	params: { maxRecipes: '2' },
-	headers: {
-		'x-rapidapi-host': 'recipesapi2.p.rapidapi.com',
-		'x-rapidapi-key': 'aca7a886e9mshee0f8c5d443f65bp1141abjsn4d18f3853312',
-	},
-};
+export const funkcija = async (imejela) => {
+	var options = {
+		method: 'GET',
+		url: `https://recipesapi2.p.rapidapi.com/recipes/${imejela}`,
+		params: { maxRecipes: '1' },
+		headers: {
+			'x-rapidapi-host': 'recipesapi2.p.rapidapi.com',
+			'x-rapidapi-key': 'aca7a886e9mshee0f8c5d443f65bp1141abjsn4d18f3853312',
+		},
+	};
 
-axios
-	.request(options)
-	.then(function (response) {
-		console.log(response.data);
-	})
-	.catch(function (error) {
-		console.error(error);
-	});
+	return await axios.request(options);
+};
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,19 @@ import Hero from '../components/Home/Hero';
 import Services from '../components/Home/Services';
 import Join from '../components/Home/Join';
 import Footer from '../components/partials/Footer';
+import React, { useEffect, useState } from 'react';
+
+import { funkcija } from '../api/getRecepies';
+
 export default function Home() {
+	const [recept1, setRecept1] = useState({});
+
+	useEffect(async () => {
+		setRecept1(await funkcija('soup'));
+	}, []);
+
+	console.log(recept1);
+
 	return (
 		<>
 			<Header />
