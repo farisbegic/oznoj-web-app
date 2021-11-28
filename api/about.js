d2b1544557d7bae25e60a293fe6e6b2c2e6f5c14
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-28 12:45:48 +0100

    about me Zlatko
    about me Zlatko

--- api/about.js
+++ api/about.js
@@ -1,49 +1,56 @@
 export const aboutData = [
-    {
-        id: '1',
-        img: 'https://ca.slack-edge.com/TD052AKUP-U01CAF96MUP-422213ab8fb9-512',
-        name: 'Zlatko Marjanović',
-        role: 'Developer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices in et at gravida morbi ac eget dictum a.',
-        age: '21',
-        location: 'Travnik',
-        profession: 'Developer',
-        university: 'University',
-        experience: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam at venenatis cum sed at lectus libero tempus, massa. Enim amet elit turpis nunc.'
-    },
-    {
-        id: '3',
-        img: 'https://media-exp1.licdn.com/dms/image/C4D03AQGPuVIf_tEQrg/profile-displayphoto-shrink_800_800/0/1630098570810?e=1643846400&v=beta&t=QRHitJiRdbEc7oxjicPmTZpzUc6pK6pYtMQf79FBBPA',
-        name: 'Amna Spahić',
-        role: 'Graphic Designer',
-        quote: 'Accomplishing goals is not success. How much you expand in the process is.',
-        location: 'Sarajevo',
-        profession: 'Graphic Design Student',
-        university: 'IBU',
-        experience: 'Worked on several projects using Adobe softwares. Junior Scholar in BH Futures Foundation. Participated BESTMostar hackathon.'
-    },
-    {
-        id: '3',
-        img: 'https://ca.slack-edge.com/TD052AKUP-U01CAF96MUP-422213ab8fb9-512',
-        name: 'Zlatko Marjanović',
-        role: 'Developer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices in et at gravida morbi ac eget dictum a.',
-        age: '21',
-        location: 'Travnik',
-        profession: 'Developer',
-        university: 'University',
-        experience: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam at venenatis cum sed at lectus libero tempus, massa. Enim amet elit turpis nunc.'
-    },
-    {
-        id: '3',
-        img: 'https://ca.slack-edge.com/TD052AKUP-U01CAF96MUP-422213ab8fb9-512',
-        name: 'Zlatko Marjanović',
-        role: 'Developer',
-        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices in et at gravida morbi ac eget dictum a.',
-        age: '21',
-        location: 'Travnik',
-        profession: 'Developer',
-        university: 'University',
-        experience: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam at venenatis cum sed at lectus libero tempus, massa. Enim amet elit turpis nunc.'
-    }
+	{
+		id: '1',
+		img: 'https://ca.slack-edge.com/TD052AKUP-U01CAF96MUP-422213ab8fb9-512',
+		name: 'Zlatko Marjanović',
+		role: 'Developer',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices in et at gravida morbi ac eget dictum a.',
+		age: '21',
+		location: 'Travnik',
+		profession: 'Developer',
+		university: 'University',
+		experience:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam at venenatis cum sed at lectus libero tempus, massa. Enim amet elit turpis nunc.',
+	},
+	{
+		id: '3',
+		img: 'https://media-exp1.licdn.com/dms/image/C4D03AQGPuVIf_tEQrg/profile-displayphoto-shrink_800_800/0/1630098570810?e=1643846400&v=beta&t=QRHitJiRdbEc7oxjicPmTZpzUc6pK6pYtMQf79FBBPA',
+		name: 'Amna Spahić',
+		role: 'Graphic Designer',
+		quote:
+			'Accomplishing goals is not success. How much you expand in the process is.',
+		location: 'Sarajevo',
+		profession: 'Graphic Design Student',
+		university: 'IBU',
+		experience:
+			'Worked on several projects using Adobe softwares. Junior Scholar in BH Futures Foundation. Participated BESTMostar hackathon.',
+	},
+	{
+		id: '3',
+		img: 'https://ca.slack-edge.com/TD052AKUP-U01CAF96MUP-422213ab8fb9-512',
+		name: 'Zlatko Marjanović',
+		role: 'Developer',
+		quote:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ultrices in et at gravida morbi ac eget dictum a.',
+		age: '21',
+		location: 'Travnik',
+		profession: 'Developer',
+		university: 'University',
+		experience:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quam at venenatis cum sed at lectus libero tempus, massa. Enim amet elit turpis nunc.',
+	},
+	{
+		id: '3',
+		img: 'https://ca.slack-edge.com/TD052AKUP-U01CAF96MUP-422213ab8fb9-512',
+		name: 'Zlatko Marjanović',
+		role: 'Full-stack Developer | Salesforce Architect | STEM & Entrepreneurship ',
+		quote: 'Anything that can be digitized, will be digitized. Carli Fiorina',
+		age: '20',
+		location: 'Živinice',
+		profession: 'Developer',
+		university: 'Internacionalna Poslovno Informaciona Akademija Tuzla',
+		experience:
+			'IEEE Innovation Nation Fellow, Participated in 2018 Bicom Systems Hackathon, Participated in BestMostar Hackathon, Bold 2020 Fellow, Event Coordinator, Salesforce Architect, FLS.ba web & technical support, Freelance web dev',
+	},
 ];
