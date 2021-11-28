2eb74b87b1bd57bc6e40233351267b6167cf51ef
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-28 12:47:56 +0100

    dodajem header u startnow
    dodajem header u startnow

--- pages/startnow/index.js
+++ pages/startnow/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Hero from '../../components/Home/Hero';
 import StartForm from '../../components/StartNow/StartForm';
 import Footer from '../../components/partials/Footer';
+import Header from '../../components/partials/Header';
 const startnow = () => {
 	return (
 		<>
+			<Header />
 			<Hero
 				heroTitle="Let's get you in shape!"
 				heroText='You are about to get the best meal & workout plan of your life!'
