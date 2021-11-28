4cabada18a69a6f94516aa3377f6d38dd7a55a86
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-28 14:49:09 +0100

    final 01
    final 01

--- components/StartNow/MealPlanRecommend.js
+++ components/StartNow/MealPlanRecommend.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Recipes from '../Recipes/Recipes';
 
 const MealPlanRecommend = () => {
-	return <div>Meal Plan</div>;
+	return (
+		<div>
+			<h2 style={{ fontSize: '3rem', margin: '3rem' }}>
+				Some of the healthy foods we recommend
+			</h2>
+			<Recipes />
+		</div>
+	);
 };
 
 export default MealPlanRecommend;
