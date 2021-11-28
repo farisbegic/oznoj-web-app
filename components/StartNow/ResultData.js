8877f41e23540d1075b342fd2df10bf3c9e1aff7
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-28 04:14:04 +0100

    holpak podataka
    holpak podataka

--- components/StartNow/ResultData.js
+++ components/StartNow/ResultData.js
@@ -8,25 +8,84 @@ const ResultData = ({
 	idealWeight,
 	bfPer,
 }) => {
-	console.log(macros);
 	console.log(dailyCalories);
-	console.log(idealWeight);
-	console.log(bfPer);
+	if (!macros) return 'Loading...';
+
 	return (
 		<div className='sexy_data'>
-			<h2>Your BMI Information</h2>
+			<h2>Your BMI Data</h2>
 			<div className='sexy_data_cards'>
 				<div className='sexy_data_card'>
 					<h3>Your BMI Index is</h3>
 					<h1>{BMIIndex.bmi}</h1>
 				</div>
 				<div className='sexy_data_card'>
-					<h3>Your BMI Index is</h3>
-					<h1>{BMIIndex.bmi}</h1>
+					<h3>Health</h3>
+					<h1>{BMIIndex.health}</h1>
 				</div>
 				<div className='sexy_data_card'>
-					<h3>Your BMI Index is</h3>
-					<h1>{BMIIndex.bmi}</h1>
+					<h3>Healthy BMI Range:</h3>
+					<h1>{BMIIndex.healthy_bmi_range}</h1>
+				</div>
+			</div>
+			<h2>Your Macros</h2>
+			<h1>Calories: {macros?.calorie?.toFixed(2)}</h1>
+			<div className='sexy_data_cards'>
+				<div className='sexy_data_card'>
+					<h3>Balanced Intake</h3>
+					<h4>Protein: {macros?.balanced?.protein?.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.balanced?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.balanced?.carbs?.toFixed(2)}g</h4>
+				</div>
+				<div className='sexy_data_card'>
+					<h3>Low Fat</h3>
+					<h4>Protein: {macros?.lowfat?.protein?.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.lowfat?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.lowfat?.carbs?.toFixed(2)}g</h4>
+				</div>
+				<div className='sexy_data_card'>
+					<h3>Low Carbs:</h3>
+
+					<h4>Protein: {macros?.lowcarbs?.protein.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.lowcarbs?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.lowcarbs?.carbs?.toFixed(2)}g</h4>
+				</div>
+				<div className='sexy_data_card'>
+					<h3>High Protein:</h3>
+
+					<h4>Protein: {macros?.highprotein?.protein?.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.highprotein?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.highprotein?.carbs?.toFixed(2)}g</h4>
+				</div>
+			</div>
+			<h2>Your Daily Calorie Intake</h2>
+			<h1>BMR: {macros?.calorie?.toFixed(2)}</h1>
+			<div className='sexy_data_cards'>
+				<div className='sexy_data_card'>
+					<h3>Balanced Intake</h3>
+					<h4>Protein: {macros?.balanced?.protein?.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.balanced?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.balanced?.carbs?.toFixed(2)}g</h4>
+				</div>
+				<div className='sexy_data_card'>
+					<h3>Low Fat</h3>
+					<h4>Protein: {macros?.lowfat?.protein?.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.lowfat?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.lowfat?.carbs?.toFixed(2)}g</h4>
+				</div>
+				<div className='sexy_data_card'>
+					<h3>Low Carbs:</h3>
+
+					<h4>Protein: {macros?.lowcarbs?.protein.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.lowcarbs?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.lowcarbs?.carbs?.toFixed(2)}g</h4>
+				</div>
+				<div className='sexy_data_card'>
+					<h3>High Protein:</h3>
+
+					<h4>Protein: {macros?.highprotein?.protein?.toFixed(2)}g</h4>
+					<h4>Fat: {macros?.highprotein?.fat?.toFixed(2)}g</h4>
+					<h4>Carbs: {macros?.highprotein?.carbs?.toFixed(2)}g</h4>
 				</div>
 			</div>
 		</div>
