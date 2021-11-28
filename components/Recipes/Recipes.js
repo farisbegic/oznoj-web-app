14807ac86d1c5d63d39897d2c20519b1642a04c6
farisbegic <faris.begic@stu.ssst.edu.ba>
2021-11-28 02:13:35 +0100

    Implemented spinner while data is not loaded

--- components/Recipes/Recipes.js
+++ components/Recipes/Recipes.js
@@ -3,9 +3,11 @@ import Product from '../partials/Product';
 import { supplementData } from '../../api/supplements';
 import { funkcija } from '../../api/getRecepies';
 import Recipe from "../partials/Recipe";
+import Loader from "react-loader-spinner";
 
 const Recipes = () => {
 	const [recept, setRecept] = useState([]);
+	const [load, setLoad] = useState(false);
 
 	useEffect(async () => {
 		setRecept(await funkcija());
@@ -15,7 +17,7 @@ const Recipes = () => {
 		<div className='recipes'>
 			{recept.data ? (recept.data.hits.map((recipe) => (
 				<Recipe image={recipe.recipe.image} name={recipe.recipe.label} protein={recipe.recipe.totalNutrients.PROCNT.quantity} carbohydrates={recipe.recipe.totalNutrients.CHOCDF.quantity} fat={recipe.recipe.totalNutrients.FAT.quantity} url={recipe.recipe.url}/>
-			))) : ''}
+			))) : <Loader type="TailSpin" color="#7C9DF3" height={100} width={100}/>}
 		</div>
 	);
 };
